a02375177d13aec83c938d882d44f2523cdbe95e
unknown <igor.di.paolo@gmail.com>
2016-01-26 01:46:02 +0100

    Non funziona ...

--- src/main/java/com/coedil99/xml/CommessaSetCollectionAdapter.java
+++
@@ -1,9 +0,0 @@
-package com.coedil99.xml;
-
-import javax.xml.bind.annotation.adapters.XmlAdapter;
-
-/**
- * Created by Igor on 25/01/2016.
- */
-public class CommessaSetCollectionAdapter extends XmlAdapter {
-}
